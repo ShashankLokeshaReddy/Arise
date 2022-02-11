9fc67dec98af3cdcc60cbebadc0198c1c1b13127
Helena Graf <helena.graf@iem-extern.fraunhofer.de>
2022-02-11 15:46:59 +0100

    Change HTML to update and not create new label

--- django_prototype/home/static/home/js/fetch_request.js
+++ django_prototype/home/static/home/js/fetch_request.js
@@ -12,7 +12,7 @@ $(function () {
                     method: "GET",
                     data: {date: date},
                     success: function (data) {
-                        $('#testContainer').append(`<div><label id="addLabel" for="name">${data['selected_date']}</label></div>`);
+                        $('#server_response').html(`<div><label id="addLabel" for="name">${data['selected_date']}</label></div>`)
                     },
                     error: function (data) {
                         alert(data.toString())
