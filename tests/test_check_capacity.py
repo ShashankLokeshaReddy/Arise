2c443ede8d98d8b45e10b70e3c561f07bbcf84c3
Jonathan Brock <jonathan.brock@iem.fraunhofer.de>
2022-03-09 10:45:23 +0000

    Feature/material check

--- tests/test_check_capacity.py
+++ tests/test_check_capacity.py
@@ -18,3 +18,10 @@ def test_machine_parser():
     df2 = pd.DataFrame(data2)
     print('Tests machine parser')
     assert_frame_equal(df1, df2)
+
+
+def test_material_check():
+    test_date1 = pd.to_datetime("2016-02-16").date()
+    test_date2 = pd.to_datetime("2016-02-18").date()
+    material_check_test = cc.material_check(test_date1)
+    assert material_check_test(test_date2) == False
