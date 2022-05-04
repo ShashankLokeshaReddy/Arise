5df3f21e3b8d5b59a7448b056ccb09f47cd5ed2b
Caroline Junker <caroline.junker@iem.fraunhofer.de>
2022-05-04 10:57:46 +0200

    Delete packages from setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 setup(
     name='arise-protoype',
     version='0.0.1',
-    packages=['arise_prototype'],
-    install_requires=['pandas',
-                      'numpy',
-                      'datetime']
+    packages=['arise_prototype']
+
 )
