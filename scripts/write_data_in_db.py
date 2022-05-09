c6cd884afab53fcd7d20aef5d013102fe1fc5cd2
Caroline Junker <caroline.junker@iem.fraunhofer.de>
2022-05-09 10:21:30 +0200

    Add docstrings

--- scripts/write_data_in_db.py
+++ scripts/write_data_in_db.py
@@ -6,6 +6,8 @@ import arise_prototype.capacity_check as cc
 
 
 def write_data_to_db():
+    """ Writes data from a static table into the database
+    """
     # Load data
     start = pd.to_datetime('2019-01-01', format='%Y-%m-%d')
     end = pd.to_datetime('2021-11-30', format='%Y-%m-%d')
@@ -44,6 +46,26 @@ def write_data_to_db():
 
 
 def load_table_from_db(server, database, username, password, sql_query):
+    """Makes a request to a database and catches the answer table.
+
+    Parameters
+    ----------
+    server : string
+        Server adress
+    database : string
+        Name of database
+    username : string
+        Username for db login
+    password : string
+        Password for db login
+    sql_query : string
+        SQL query
+
+    Returns
+    -------
+    pd.Dataframe
+        Received table in dataframe format
+    """
     db_info = 'DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password+''
     cnxn = pyodbc.connect(db_info)
     cursor = cnxn.cursor()
@@ -55,6 +77,8 @@ def load_table_from_db(server, database, username, password, sql_query):
 
 
 def check_order_table():
+    """Checks if data is written into db
+    """
     sql_query = """
         select AKNR, Fefco_Teil, ArtNr_Teil, Menge_Soll, LTermin, KndNr, Suchname, TeilNr, ID_Druck, Bogen_Laenge_Brutto, 
             Bogen_Breite_Brutto, MaschNr, Ruestzeit_Soll, Laufzeit_Soll, Lieferdatum_Rohmaterial
